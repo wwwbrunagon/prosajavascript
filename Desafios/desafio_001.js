bdc83cae83b6d06c278ae87a8f015e376fb2d664
Bruna Gonçalves <wwwbrunagon@gmail.com>
2020-01-16 00:28:11 +0000

    desafio 001

---
+++ Desafios/desafio_001.js
@@ -0,0 +1,23 @@
+
+//melhorar esse código para que a frase final seja: 
+//${assuntos[index]} é o ${cronograma[index]} assunto do pŕoximo Encontro com JS
+
+// revisão de classe é o 1º assunto do próximo Encontro com JS
+
+//comentar esse código e fazer outrro código com sua autoria
+
+const programacaoHoje = {
+  cronograma: ['1º', '2º', '3º'],
+  assuntos: ['revisão de classe', 'template string', 'objeto literal'],
+  resumo: function() {
+    this.assuntos.forEach(r => {
+      console.log(`${r} é assunto do Encontro com JS de hoje`);
+    })
+  }
+}
+
+programacaoHoje.resumo('ASSUNTO 1', 'ASSUNTO 2'), ...
+
+
+
+
